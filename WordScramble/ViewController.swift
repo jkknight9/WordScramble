1a88db52230509549cbcf93a1d1f6710967537a1
Jack Knight <jkknight9@gmail.com>
2020-07-27 13:29:09 -0600

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Jack Knight on 7/27/20.
+//  Copyright © 2020 Jack Knight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
